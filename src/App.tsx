db3656b5590915d669306d6e4aa8e3d645bb3f21
Tirthan Prajapati <tirthanbhuvan@gmail.com>
2024-12-21 21:40:08 +0530

    small updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Sidebar } from './components/Sidebar';
 import { Dashboard } from './pages/Dashboard';
 import { Transactions } from './pages/Transactions';
+import './App.css'; // Add this line to include App CSS
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex min-h-screen bg-gray-50">
+      <div className="app">
         <Sidebar />
         <main className="flex-1 ml-64">
           <Routes>
